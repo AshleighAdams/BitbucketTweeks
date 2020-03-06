9c74a2305aec676bbf87d6ee2dbb8b08b662302c
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2020-03-06 11:23:06 +0000

    Tidied up code a little.

--- js/injector.js
+++ js/injector.js
@@ -1,28 +1,31 @@
-if(window.location.hostname.startsWith("bitbucket.")) {
+
+var is_bitbucket = $("#logo").hasClass("bitbucket-header-logo")
+
+if (is_bitbucket) {
 	$(".pull-request-activity-content").hide()
-	chrome.storage.sync.get(['chrono', 'shrink_lbl'], function(settings) {
+	setTimeout(function () {
+		$(".pull-request-activity-content").show()
+	}, 200)
+
+	chrome.storage.sync.get(["chrono", "shrink_lbl"], function(settings) {
 
 		if(settings.chrono) {
-			$('head').append(
-				$('<link>')
+			$("head").append(
+				$("<link>")
 					.attr("rel","stylesheet")
 					.attr("type","text/css")
-					.attr("href", chrome.extension.getURL('css/chronological.css'))
+					.attr("href", chrome.extension.getURL("css/chronological.css"))
 			);
 		}
 
 		if(settings.shrink_lbl) {
-			$('head').append(
-				$('<link>')
+			$("head").append(
+				$("<link>")
 					.attr("rel","stylesheet")
 					.attr("type","text/css")
-					.attr("href", chrome.extension.getURL('css/shrink_lbl.css'))
+					.attr("href", chrome.extension.getURL("css/shrink_lbl.css"))
 			);
 		}
-
-		setTimeout(function() {
-			$(".pull-request-activity-content").show()
-		}, 100)
 
 	});
 }
